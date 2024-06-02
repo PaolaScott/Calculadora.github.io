0f04aef46cd176818ea1ad851919a5c0cb86a6fb
PaolaScott <onyxpaola5@gmail.com>
2024-06-01 22:07:03 -0600

    cambios

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 document.addEventListener("DOMContentLoaded", () => {
     const pantalla = document.getElementById("valor-actual");
     const botones = document.querySelectorAll(".btn");
@@ -54,60 +53,3 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
 });
-=======
-document.addEventListener("DOMContentLoaded", () => {
-    const pantalla = document.getElementById("valor-actual");
-    const botones = document.querySelectorAll(".btn");
-
-    botones.forEach(boton => {
-        boton.addEventListener("click", () => {
-            const botonApretado = boton.value;
-
-            if (boton.id === "c") {
-                limpiarPantalla();
-                return;
-            }
-
-            if (boton.id === "borrar") {
-                borrarUltimoCaracter();
-                return;
-            }
-
-            if (boton.id === "igual") {
-                calcularResultado();
-                return;
-            }
-
-            actualizarPantalla(botonApretado);
-        });
-    });
-
-    function limpiarPantalla() {
-        pantalla.textContent = "0";
-    }
-
-    function borrarUltimoCaracter() {
-        if (pantalla.textContent.length === 1 || pantalla.textContent === "Error!") {
-            pantalla.textContent = "0";
-        } else {
-            pantalla.textContent = pantalla.textContent.slice(0, -1);
-        }
-    }
-
-    function calcularResultado() {
-        try {
-            pantalla.textContent = eval(pantalla.textContent.replace('÷', '/').replace('X', '*'));
-        } catch {
-            pantalla.textContent = "Error!";
-        }
-    }
-
-    function actualizarPantalla(botonApretado) {
-        if (pantalla.textContent === "0" || pantalla.textContent === "Error!") {
-            pantalla.textContent = botonApretado;
-        } else {
-            pantalla.textContent += botonApretado;
-        }
-    }
-});
->>>>>>> dd046dd (Cambios en index)
